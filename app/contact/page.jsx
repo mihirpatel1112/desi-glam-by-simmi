c8c47eecb5d73d1b03bb1b2e642d1593c266cd63
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2025-01-04 00:25:55 +1100

    Contact page error fixed

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Footer from "@/components/Footer"
 
 export default function Contact() {
@@ -12,6 +12,12 @@ export default function Contact() {
     time: '',
     message: ''
   })
+
+  const [iframeHeight, setIframeHeight] = useState("300")
+
+  useEffect(() => {
+    setIframeHeight(window.innerWidth >= 768 ? "600" : "300")
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -62,7 +68,7 @@ export default function Contact() {
               <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241317.11609823277!2d72.74109995644679!3d19.08219783958221!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1659342165461!5m2!1sen!2sin"
                 width="100%"
-                height={window.innerWidth >= 768 ? "600" : "300"}
+                height={iframeHeight}
                 style={{border:0}}
                 allowFullScreen=""
                 loading="lazy"
